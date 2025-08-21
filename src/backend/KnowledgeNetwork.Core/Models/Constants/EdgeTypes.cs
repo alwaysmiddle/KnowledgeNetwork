df1d34d6367fe1c9ad5e6c86285473f6afd0aa27
Shizhong Yu <ysz10000@gmail.com>
2025-08-20 21:08:38 -0400

    Reorganized the types into constant folder.

--- src/backend/KnowledgeNetwork.Core/Models/Constants/EdgeTypes.cs
+++ src/backend/KnowledgeNetwork.Core/Models/Constants/EdgeTypes.cs
@@ -33,16 +33,3 @@ public static class EdgeTypes
     public const string Aggregates = "aggregates";
     public const string FocusesOn = "focuses-on";
 }
-
-/// <summary>
-/// Categories for edge types
-/// </summary>
-public static class EdgeCategories
-{
-    public const string Structure = "structure";
-    public const string Execution = "execution";
-    public const string ControlFlow = "control-flow";
-    public const string Dependency = "dependency";
-    public const string Documentation = "documentation";
-    public const string View = "view";
-}
